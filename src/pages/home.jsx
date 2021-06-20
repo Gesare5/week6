6ab337d326c16aeaaa129444bc83c8c7500c0ab5
Ivy Gesare <gesare.nyangweso@gmail.com>
2021-06-20 21:48:44 +0300

    react note-app initial draft

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1> Welcome!  </h1>
+            <h2> This is home.</h2>
+        </div>
+    );
+}
+
+export default Home;
